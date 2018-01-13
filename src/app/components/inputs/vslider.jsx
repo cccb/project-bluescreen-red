080eb00925bb3ceeeb5337c5c79abb04f494a8d9
Matthias Hannig <matthias@hannig.cc>
2018-01-13 18:37:54 +0100

    basic vslider drawing

--- src/app/components/inputs/vslider.jsx
+++ src/app/components/inputs/vslider.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import {Component} from 'react'
 
 
-export default class Slider extends Component {
+export default class VSlider extends Component {
 
   componentDidMount() {
     this.init();
@@ -24,15 +24,31 @@ export default class Slider extends Component {
   }
 
   // Drawing routines
-  drawTicks(ctx, x, height, num) {
+  drawScale(ctx, x, width, height, num) {
+    const dy = height / num;
+    for (let i = 1; i < num; i++) {
+      let y = i * dy;
 
+      ctx.fillStyle = "#aaaaaa";
+      ctx.fillRect(x, y, width, 2);
+    }
   }
 
-  drawHandle(ctx, x, width, height) {
-
+  drawHandle(ctx, x, y, width, height) {
+    ctx.fillStyle = "#aaaaaa";
+    ctx.fillRect(x,
+                 y - 0.5 * height,
+                 width, height);
+    ctx.fillStyle = "#ffffff";
+    ctx.fillRect(x,
+                 y - 0.5 * height,
+                 width, height * 0.5);
   }
 
   drawBase(ctx, x, width, height) {
+    ctx.fillStyle = "#aaaaaa";
+    ctx.fillRect(x - 0.5 * width, 0,
+                 width, height);
   }
 
   draw() {
@@ -40,9 +56,23 @@ export default class Slider extends Component {
     const ctx = canvas.getContext("2d");
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    this.drawBase(ctx, canvas.width / 2, 4, canvas.height);
+    this.drawScale(ctx,
+                   0.5 * canvas.width + 10,
+                   10,
+                   canvas.height, 
+                   20);
+    this.drawScale(ctx,
+                   0.5 * canvas.width - 20,
+                   10,
+                   canvas.height, 
+                   20);
+    this.drawHandle(ctx,
+                    0,
+                    59, // calculate from value
+                    canvas.width,
+                    15);
 
-    ctx.fillStyle = "#00ff00";
-    ctx.fillRect(0,0, 40, 100);
   }
 
   render() {
