eeec83709e30159bb29f3e33f74ab97a184eb88e
Matthias Hannig <matthias@hannig.cc>
2018-01-28 22:13:33 +0100

    handle set sources

--- src/app/pages/main-audio/reducer.jsx
+++ src/app/pages/main-audio/reducer.jsx
@@ -1,6 +1,23 @@
 
 import {SET_MASTER_VOLUME,
-        MQTT_SET_LEVEL_SUCCESS} from './actions'
+
+        MQTT_SET_LEVEL_SUCCESS,
+        MQTT_SET_SOURCE_SUCCESS,
+
+        MQTT_GET_SOURCES_SUCCESS,
+        MQTT_GET_LEVELS_SUCCESS,
+        MQTT_GET_TOGGLES_SUCCES} from './actions'
+
+
+import {MAIN_SOURCE,
+      
+        MAIN_MASTER_LEVEL,
+
+        MAIN_SOURCE_DESK,
+        MAIN_SOURCE_HDMI,
+        MAIN_SOURCE_SONIC,
+        MAIN_SOURCE_FOH} from 'config/mappings/audio'
+
 
 const initialState = {
   masterVolumeLevel: 0,
@@ -19,7 +36,7 @@ const initialState = {
 
 
 function _handleSetLevel(state, levelId, value) {
-  if(levelId == 1) { // MasterVolume
+  if(levelId == MAIN_MASTER_LEVEL) { // MasterVolume
     // Remap
     let masterVolumeLevel = (value / 255.0) * 100;
     return Object.assign({}, state, {
@@ -31,6 +48,29 @@ function _handleSetLevel(state, levelId, value) {
 }
 
 
+function _handleGetSources(state, result) {
+  let next = Object.assign({}, state);
+
+  for (let source of result) {
+    if (source.id == MAIN_SOURCE) {
+      next.sourceId = source.value; 
+    }
+  }
+
+  return next;
+}
+
+function _handleSetSource(state, source, value) {
+  if (source != MAIN_SOURCE) {
+    return state;
+  }
+
+  let next = Object.assign({}, state);
+  next.sourceId = value;
+
+  return next;
+}
+
 export default function reducer(state=initialState, action) {
   switch(action.type) {
     case SET_MASTER_VOLUME:
@@ -39,8 +79,15 @@ export default function reducer(state=initialState, action) {
       });
     case MQTT_SET_LEVEL_SUCCESS:
       return _handleSetLevel(state,
-                             action.payload.level_id,
+                             action.payload.id,
                              action.payload.value);
+    case MQTT_SET_SOURCE_SUCCESS:
+      return _handleSetSource(state,
+                              action.payload.id,
+                              action.payload.value);
+    case MQTT_GET_SOURCES_SUCCESS:
+      return _handleGetSources(state,
+                               action.payload);
   }
 
   return state;
