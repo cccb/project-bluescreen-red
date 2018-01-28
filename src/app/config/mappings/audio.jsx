d38a9f84eb24891f76bd3ad67cdb9f74fbdb09f1
Matthias Hannig <matthias@hannig.cc>
2018-01-28 21:53:19 +0100

    added audio mappingl

---
+++ src/app/config/mappings/audio.jsx
@@ -0,0 +1,18 @@
+
+
+export const MAIN_MASTER_LEVEL = 1;
+export const MAIN_DELAY_LEVEL = 3;
+export const MAIN_DELAY_LEVEL_LEFT = 3;
+export const MAIN_DELAY_LEVEL_RIGHT = 7;
+export const MAIN_BASS_LEVEL = 5;
+export const MAIN_BAR_LEVEL = 2;
+
+export const MAIN_SOURCE = 2;
+export const MAIN_SOURCE_LEFT = 2;
+export const MAIN_SOURCE_RIGHT = 7;
+
+export const MAIN_SOURCE_DESK = 1;
+export const MAIN_SOURCE_HDMI = 2;
+export const MAIN_SOURCE_SONIC = 3;
+export const MAIN_SOURCE_FOH = 4;
+
