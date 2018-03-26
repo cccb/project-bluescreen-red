90c3d684d2b9d6f2517bc538d6eb0717dfbb2558
Matthias Hannig <matthias@hannig.cc>
2018-03-26 21:53:16 +0200

    started soundboard project

--- src/app/reducers/app-reducer.jsx
+++ src/app/reducers/app-reducer.jsx
@@ -8,6 +8,7 @@ import mainHallReducer from 'pages/main-hall/reducer'
 import mainAudioReducer from 'pages/main-audio/reducer'
 import mainHdmiReducer from 'pages/main-hdmi/reducer'
 import lightsReducer from 'pages/lights/reducer'
+import soundboardReducer from 'pages/soundboard/reducer'
 import configReducer from 'utils/config/reducer'
 
 export default combineReducers({
@@ -16,6 +17,7 @@ export default combineReducers({
   mainAudio: mainAudioReducer,
   mainHdmi: mainHdmiReducer,
   lights: lightsReducer,
+  soundboard: soundboardReducer,
   config: configReducer
 });
 
