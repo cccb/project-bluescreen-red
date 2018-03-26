c98ab7eaf6d1b1ada78624e0efac943993942790
Matthias Hannig <matthias@hannig.cc>
2018-03-26 22:02:46 +0200

    added action creators

--- src/app/pages/soundboard/actions.jsx
+++ src/app/pages/soundboard/actions.jsx
@@ -27,3 +27,36 @@ export const MQTT_SAMPLE_STOP_SUCCESS =
 export const MQTT_SAMPLE_STOP_ERROR =
              "@@mqtt/v1/mainhall/sampler/SAMPLE_STOP_ERROR";
 
+
+export function mqttGroupsListRequest() {
+  return {
+    type: MQTT_GROUPS_LIST_REQUEST,
+    payload: {},
+  };
+}
+
+export function mqttSamplesListRequest(group="*") {
+  return {
+    type: MQTT_GROUPS_LIST_REQUEST,
+    payload: {
+      group: group
+    },
+  };
+}
+
+export function mqttSampleStartRequest(sampleId) {
+  return {
+    type: MQTT_SAMPLE_START_REQUEST,
+    payload: {
+      sample_id: sampleId
+    },
+  };
+}
+
+export function mqttSampleStopRequest(sampleId) {
+  return {
+    type: MQTT_SAMPLE_STOP_REQUEST,
+    payload: {},
+  }
+}
+
--- src/app/pages/soundboard/reducer.jsx
+++ src/app/pages/soundboard/reducer.jsx
@@ -25,6 +25,7 @@ const initialState = {
 
 
 export default function reducer(state=initialState, action) {
+
   return state; 
 }
 
