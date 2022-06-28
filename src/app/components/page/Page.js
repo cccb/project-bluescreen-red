103dc6a74c46f46dba03a3a0b0d1cdf19c1f8bdf
Annika Hannig <annika@hannig.cc>
2022-06-28 17:59:09 +0000

    added main layout

---
+++ src/app/components/page/Page.js
@@ -0,0 +1,60 @@
+
+import { Outlet }
+  from 'react-router-dom';
+
+
+/**
+ * TitleBar is the upper bar on the screen
+ */
+const TitleBar = () => {
+  return (
+    <div className="nav-titlebar">
+      <div className="nav-title">
+        CCCB Bluescreen Pink
+      </div>
+      <div className="nav-version">
+        0.42.3
+      </div>
+    </div>
+  );
+}
+
+
+/**
+ * Footer is the bar at the bottom of the screen
+ */
+const Footer = () => {
+  return (
+    <div className="nav-footer">
+      <div className="nav-copyright">
+        (c) 1989 Gesellschaft f&uuml;r geschlossene
+        Software Entwicklung e.V.
+      </div>
+      <div className="nav-license">
+        Unregistered Copy
+      </div>
+    </div>
+  );
+}
+
+/**
+ * Page provides the main application layout
+ */
+const Page = () => {
+  return (
+    <div className="app">
+      <TitleBar />
+      <div className="app-main row">
+        <div className="app-left col-md-2">
+          <Sidebar />
+        </div>
+        <div className="app-content col-md-10">
+          <Outlet />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Page;
